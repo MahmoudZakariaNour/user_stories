527559156a98d3d244850b54d294ee63b8098a9e
Ma7moudHelmi <mahmoudhelmy31@gmail.com>
2022-04-03 15:13:46 +0200

    edit

---
+++ Components/Authers.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export function Auther({ data }) {
+  return (
+    <div>
+      <h3>{data.name}</h3>
+      <Link to={`/Home/${data.id}`}>READ MORE</Link>
+    </div>
+  );
+}
