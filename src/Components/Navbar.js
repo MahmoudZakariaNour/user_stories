0b9514ac3105f3236ebbbc22ba993fbc8e77c2ee
Ma7moudHelmi <mahmoudhelmy31@gmail.com>
2022-04-04 23:16:54 +0200

    New

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,7 +8,7 @@ export function Navbar() {
       <a class="navbar-brand" href="/" style={{color:"black"}}>API TEST</a>
     </div>
     <ul class="nav navbar-nav navbar-right">
-      <li class="active"><a href="/home" style={{color:"black"}}>Home</a></li>
+      <li class="active"><a href="/home" style={{color:"black"}}>Author</a></li>
       <li><a href="/posts" style={{color:"black"}}>Posts</a></li>
     </ul>
 
